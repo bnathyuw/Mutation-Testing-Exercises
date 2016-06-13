df7b43ae965de240aac6b912ec4c0f3470a000f8
Matthew Butt <matthew@codurance.com>
2016-06-13 17:43:15 +0100

    An example of a web api for kitten lovers.

---
+++ MutationTestingExercises/Exercise2WebApi/RequestHandlerResolver.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace MutationTestingExercises.Exercise2WebApi
+{
+    public class RequestHandlerResolver
+    {
+        public RequestHandler Resolve(HttpMethod httpMethod, string path)
+        {
+            if (httpMethod == HttpMethod.Post && path == "kittens/")
+                return new CreateKittenRequestHandler();
+
+            if (httpMethod == HttpMethod.Get && new Regex("^kittens/\\w+$").IsMatch(path))
+                return new RetrieveKittenRequestHandler();
+
+            return new NotFoundHandler();
+        }
+    }
+
+    public interface RequestHandler
+    {
+    }
+
+    public class CreateKittenRequestHandler : RequestHandler
+    {
+    }
+
+    public class RetrieveKittenRequestHandler : RequestHandler
+    {
+    }
+
+    public class NotFoundHandler : RequestHandler
+    {
+    }
+}
---
+++ MutationTestingExercises/Exercise2WebApi/RequestHandlerResolverShould.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using NUnit.Framework;
+
+namespace MutationTestingExercises.Exercise2WebApi
+{
+    [TestFixture]
+    public class RequestHandlerResolverShould
+    {
+        private RequestHandlerResolver _requestHandlerResolver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _requestHandlerResolver = new RequestHandlerResolver();
+        }
+
+        [Test]
+        public void Resolve_a_create_kitten_request()
+        {
+            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Post, "kittens/");
+
+            Assert.That(requestHandler, Is.TypeOf<CreateKittenRequestHandler>());
+        }
+
+        [Test]
+        public void Resolve_a_retrieve_kitten_request()
+        {
+            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Get, "kittens/fluffles");
+
+            Assert.That(requestHandler, Is.TypeOf<RetrieveKittenRequestHandler>());
+        }
+
+        [Test]
+        public void Resolve_a_not_found_handler_for_an_unrecognised_request()
+        {
+            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Put, "puppies/");
+
+            Assert.That(requestHandler, Is.TypeOf<NotFoundHandler>());
+        }
+    }
+
+
+}
